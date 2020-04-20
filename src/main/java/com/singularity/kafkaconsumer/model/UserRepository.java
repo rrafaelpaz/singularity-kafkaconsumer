5168909d5105a25968267711a5b29adcdc90d2f5
Rafael Paz <rafael.paz@service.nsw.gov.au>
2020-04-21 08:23:21 +1000

    datastore dependencies

--- src/main/java/com/singularity/kafkaconsumer/model/UserRepository.java
+++ src/main/java/com/singularity/kafkaconsumer/model/UserRepository.java
@@ -3,7 +3,7 @@ package com.singularity.kafkaconsumer.model;
 
 import org.springframework.cloud.gcp.data.datastore.repository.DatastoreRepository;
 
-public class UserRepository extends DatastoreRepository<User, Long> {
+public interface UserRepository extends DatastoreRepository<User, Long> {
 
 
 
